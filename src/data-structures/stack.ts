c1cfa9e5118bf908472a3c4ed9a50948a787b7d8
Syed Vilayat Ali Rizvi <vilayatcodemysite@gmail.com>
2021-12-29 02:04:55 +0530

    Added linear queues and stacks

---
+++ src/data-structures/stack.ts
@@ -0,0 +1,36 @@
+import linkedList from "./linkedList";
+
+// implementing Stack data structure using linked lists 
+export default class Stack<T>{
+    top: any;
+    size: number;
+    list: linkedList<T>;
+    constructor(){
+        this.top = null;
+        this.size = 0;
+        this.list = new linkedList<T>();
+    }
+    push(data: T){
+        this.list.insertAtIndex(data, 0);
+        this.top = this.list.head;
+        this.size++;
+    }
+    pop(){
+        if(this.size === 1){
+            this.list.removeAtEnd();
+            this.top = null;
+            this.size--;
+        }else{
+            this.list.removeAtEnd();
+            this.size--;
+        }
+
+    }
+    showTop(index: number){
+        return this.top;
+    }
+    showStack(){
+        const arr = this.list.showList();
+        return arr;
+    }
+}
